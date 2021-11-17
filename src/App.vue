538f74919f2c2662de740c6fb7cdcd9ffa7f15db
vulcao <vulcao@gmail.com>
2021-11-16 22:58:34 -0300

    teste de comit, ver se consigo baixar isso

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Menu />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Menu
   }
 }
 </script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="menu">
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/page">Pagina</a>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+<style scoped>
+#menu {
+    display: flex;
+    
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+</style>
